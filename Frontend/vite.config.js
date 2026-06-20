3568926153160928b6c63e3008046229254d9df2
Mehtab <mehtabaliali255@gmail.com>
2026-06-20 21:21:06 +0530

    changes in vite config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build', // Tells Vite to output to "build" instead of "dist"
+  },
 })
